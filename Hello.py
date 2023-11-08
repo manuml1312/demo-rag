664ba448180f946f4de7bd3387a60bdb277104b8
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-08 16:11:53 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,51 +1,44 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import streamlit as st
-from streamlit.logger import get_logger
-
-LOGGER = get_logger(__name__)
+# Second
+import streamlit as st 
+import os
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.embeddings import HuggingFaceEmbedding
+from llama_index import ServiceContext
+from llama_index.llms import Replicate
 
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+REPLICATE_API_TOKEN = "r8_VIpRfodHy75ZM7GUguQM56Zz44Sa4G10p4Eku"
 
-    st.write("# Welcome to Streamlit! 👋")
+st.title("📝 File Q&A ") 
+uploaded_file = st.file_uploader("Upload an article", type=("txt", "md")) 
+question = st.text_input(
+    "Ask something about the article",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
 
-    st.sidebar.success("Select a demo above.")
+llama2_7b_chat = "meta/llama-2-7b-chat:8e6975e5ed6174911a6ff3d60540dfd4844201974602551e10e9e87ab143d81e"
+llm = Replicate(
+    model=llama2_7b_chat,
+    temperature=0.01,
+    additional_kwargs={"top_p": 1, "max_new_tokens": 300},
+)
 
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 
+service_context = ServiceContext.from_defaults(
+    llm=llm, embed_model=embed_model
+)
 
-if __name__ == "__main__":
-    run()
+documents = SimpleDirectoryReader("/content/").load_data()
+index = VectorStoreIndex.from_documents(
+    documents, service_context=service_context
+)
+
+# client = anthropic.Client(api_key=anthropic_api_key)
+index.storage_context.persist()
+query_engine = index.as_query_engine(service_context=service_context)
+response=query_engine.query("What is chain of thought fine tuning mixture?")
+
+st.write("### Answer")
+st.write(response)

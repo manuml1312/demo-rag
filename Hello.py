8557cab2ddc74294e414a3238c9410953728a5dd
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-08 17:19:44 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -35,8 +35,8 @@ service_context = ServiceContext.from_defaults(
 documents = SimpleDirectoryReader(input_dir="./data").load_data() 
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+# if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+#         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})

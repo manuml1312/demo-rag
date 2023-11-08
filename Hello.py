81756427914aeac74ca32ab07cce7e99477aa6ea
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-08 20:56:26 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,12 +6,8 @@ from llama_index.embeddings import HuggingFaceEmbedding
 from llama_index import ServiceContext
 from llama_index.llms import Replicate
 from llama_index import download_loader
-# pdf=download_loader('PDFReader')
-# from llama_hub.github_repo import GithubRepositoryReader, GithubClient,PDFReader
 
 os.environ["REPLICATE_API_TOKEN"] = "r8_VIpRfodHy75ZM7GUguQM56Zz44Sa4G10p4Eku"
-REPLICATE_API_TOKEN = "r8_VIpRfodHy75ZM7GUguQM56Zz44Sa4G10p4Eku"
-github_token="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"
 
 st.title("📝 File Q&A ") 
 # uploaded_file = st.file_uploader("Upload an article", type=("txt", "md","pdf")) 
@@ -36,19 +32,6 @@ embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 service_context = ServiceContext.from_defaults(
     llm=llm, embed_model=embed_model)
 
-# github_client = GithubClient(github_token)
-# loader = GithubRepositoryReader(
-#     github_client,
-#     owner =                  "manuml1312",
-#     repo =                   "demo-rag",
-#     filter_directories =     (["data"], GithubRepositoryReader.FilterType.INCLUDE),
-#     filter_file_extensions = ([".pdf"], GithubRepositoryReader.FilterType.INCLUDE),
-#     verbose =                True,
-#     concurrent_requests =    10,
-#     use_parser=False
-# )
-# docs = loader.load_data(commit_sha="99c9dd06122bdebb8108ce39d320b07d04bc5c99")
-
 reader = SimpleDirectoryReader(input_dir="./data",recursive=True)
 documents=reader.load_data() 
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
@@ -59,12 +42,15 @@ if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-# query_engine = index.as_query_engine(service_context=service_context)
-# st.write("pre")
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+st.write("pre")
 response=st.session_state.chat_engine.chat(prompt)
 
 st.write("### Answer")
-st.write(response)
+st.write(response.response)
 
-message={'role':'assistant','content':response}
+message={'role':'assistant','content':response.response}
 st.session_state.messages.append(message)

0baf21b1235a9f2f2e773b5c28b3daded6b9628d
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-08 22:55:36 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -8,7 +8,7 @@ from llama_index import ServiceContext
 from llama_index.llms import OpenAI
 
 openai.api_key = st.secrets.openai_key #
-openai.api_key=os.envget("openai_key")  #st.secrets.openai_key
+# openai.api_key=os.envget("openai_key")  #st.secrets.openai_key
 
 st.title("📝 File Q&A ") 
 

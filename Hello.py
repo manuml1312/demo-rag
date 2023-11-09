4bb20399c3bbea585d5dc184455dc2c3729ba523
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 13:42:03 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -9,16 +9,16 @@ from llama_index.llms import OpenAI
 
 openai.api_key = st.secrets.openai_key #
 
-st.title("📝 File Q&A ") 
+st.title("📝 Material Processing Guide Chatbot ") 
 
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
         {"role": "assistant", "content": "Mention the material requirements!"}
     ]
     
-llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the Energy curable resins
-for inks and coatings manual and your job is to suggest relatable materials from the document as per the question. 
-Assume that all questions are related to energy curable resins for inks and coatings. Keep your answers accurate and based on 
+llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the the material processing guide which
+      are mentioned in the supplied document.Answer the questions asked about the processing of the material technically as per the supplied
+      facts.Dont summarise the answers,keep them in detail and technical. Keep your answers accurate and based on 
                    facts – do not hallucinate features.""")
 
 service_context = ServiceContext.from_defaults(llm=llm) 
@@ -29,10 +29,10 @@ index = VectorStoreIndex.from_documents(documents, service_context=service_conte
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
         st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-# st.text_input("Mention your requirements, based on which I can suggest materials",placeholder="Your requirements here")
+# st.text_input("Mention your requirements, based on which I can suggest materials",placeholder="Your processing queries here")
 
 # if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-if prompt :=st.text_input("Tell me your requirements so that I can suggest the materials you are looking for?",placeholder="Your Question Here"):
+if prompt :=st.text_input("How can i help you with your material processing query?",placeholder="Your Question Here"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
 # If last message is not from assistant, generate a new response

18720ae9bacc1b52e6572f027d9d1545d07a44b8
manuml1312 <109586134+manuml1312@users.noreply.github.com>
2023-11-09 10:22:09 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -3,7 +3,7 @@ import streamlit as st
 import os
 import openai
 from llama_index import VectorStoreIndex, SimpleDirectoryReader , Document
-from llama_index.embeddings import HuggingFaceEmbedding  #,OpenAIEmbedding
+from llama_index.embeddings import HuggingFaceEmbedding 
 from llama_index import ServiceContext
 from llama_index.llms import OpenAI
 
@@ -14,14 +14,14 @@ st.title("📝 File Q&A ")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Scaling Instruction Finetuned Model!"}
+        {"role": "assistant", "content": "Ask me a question about the Chemical Composition of Arecanut!"}
     ]
     
-llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the Streamlit Python library and your job is to answer 
-          technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on 
+llm = OpenAI(model="gpt-3.5-turbo", temperature=0.3, system_prompt="""You are an expert on the Oil Well Integrity manual and your job is to answer 
+          all questions. Assume that all questions are related to the Well integrity of an Oil well. Keep your answers accurate and based on 
                    facts – do not hallucinate features.""")
-#embed=OpenAIEmbedding()
-service_context = ServiceContext.from_defaults(llm=llm) #,embed_model=embed)
+
+service_context = ServiceContext.from_defaults(llm=llm) 
 reader = SimpleDirectoryReader(input_dir="./data")
 documents=reader.load_data() 
 index = VectorStoreIndex.from_documents(documents, service_context=service_context)
